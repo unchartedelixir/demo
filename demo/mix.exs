a1972ee0e14a0d8291bbc77b84a63ed19c2d6cc0
Tim Mecklem <timothy@mecklem.com>
2020-09-12 13:55:18 -0400

    Separate live_chart from live_chart_web

--- demo/mix.exs
+++ demo/mix.exs
@@ -44,7 +44,7 @@ defmodule Demo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:live_chart, path: "../live_chart"}
+      {:live_chart_web, path: "../live_chart_web"}
     ]
   end
 
